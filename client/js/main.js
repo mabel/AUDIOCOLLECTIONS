8022b9f4255e796ef3003ed9fad638d19a605161
Mabel <pochta@yababay.ru>
2015-09-09 16:00:39 +0300

    More mobile adaptivity.

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,9 @@
 $(function(){
 	$.material.init();
-	var isNarrow = $(window).width()
+	var isNarrow = $(window).width() <= 320
+    var $ul = $('.list-group')
 	$.get('/cgi-bin/iso-images.sh', function(dat){
 		var arr = dat.split(/[\r\n]+/)
-		var $ul = $('.list-group')
 		arr.forEach(function(el, i){
 			if(!el) return
 			var $li = $('<li>')
@@ -26,11 +26,14 @@ $(function(){
 	})
 
 	if(isNarrow){
+        $('body').css('margin-top', '0')
+		$('nav .btn-group').removeClass('hidden').css('margin-top', '-10px').appendTo('body')
+		$('.btn-group i').css({'font-size': '40px'})
 		$('#volume')
-			.appendTo('nav')
-			.css('margin', '35px 25px 0 0')
-		$('nav .row').remove()
-		$('nav .btn-group').removeClass('hidden')
+			.appendTo('body')
+			.css('margin', '35px 25px 25px 15px')
+        $ul.appendTo('body')
+		$('main').remove()
 	}
 	else $('nav .row').removeClass('hidden')
 
@@ -43,17 +46,17 @@ $(function(){
 		},
 	});
 	volumeSlider.noUiSlider.on('change', function(e){$.get('/cgi-bin/volume.sh?' + e)})
-	$('nav .mdi-av-volume-up').click(function(){
+	$('.mdi-av-volume-up').click(function(){
 		var that = this
 		$.get('/cgi-bin/mute.sh', function(dat){
 			if(dat == 1)$(that).removeClass('mdi-av-volume-off').addClass('mdi-av-volume-up')
 			if(dat == 0)$(that).removeClass('mdi-av-volume-up').addClass('mdi-av-volume-off')
 		})
 	})
-	$('nav .mdi-av-skip-next').click(function(){
+	$('.mdi-av-skip-next').click(function(){
 		$.get('/cgi-bin/stop-single.sh', function(dat){})
 	})
-	$('nav .mdi-av-stop').click(function(){
+	$('.mdi-av-stop').click(function(){
 		$.get('/cgi-bin/stop-all.sh', function(dat){})
 	})
 })
